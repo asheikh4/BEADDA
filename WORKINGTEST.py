fb239de6fcc4a932014e39b8d413cf64e14029b5
polyominos <ddt.danieltran@gmail.com>
2025-11-08 14:21:56 -0700

    mvctestest works

--- WORKINGTEST.py
+++ WORKINGTEST.py
@@ -3,9 +3,11 @@ import numpy as np
 
 print("Looking for an OpenBCI LSL stream...")
 streams = resolve_byprop('type', 'EEG')  # Could also be 'EMG'
-inlet = StreamInlet(streams[0])
+inlet1 = StreamInlet(streams[0])
+# inlet2 = StreamInlet(streams[1])
 print("Connected!")
 
 while True:
-    sample, timestamp = inlet.pull_sample()
+    sample, timestamp = inlet1.pull_sample()
+    # sample2, timestamp = inlet2.pull_sample()
     print(f"{sample[0]}")
